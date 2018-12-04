d9adab418959403e7c9cc0737953f6de04032317
ggolish <ggolish@sycamores.indstate.edu>
2018-12-04 03:48:15 -0500

    Test script that adds users

---
+++ game/addusers.py
@@ -0,0 +1,16 @@
+
+import sys
+import websocket
+from client import Client
+
+if __name__ == "__main__":
+
+    usernames = ["harry", "sally", "bob", "jerry", "marge"]
+    clients = []
+    for i in range(len(usernames)):
+        clients.append(Client())
+        clients[-1].connect("ws://localhost:8080")
+        clients[-1].set_username(usernames[i])
+
+    while True:
+        continue
