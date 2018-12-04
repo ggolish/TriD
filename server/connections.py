fedd5e3542d4adca9423e52191f60a252a13eeeb
ggolish <ggolish@sycamores.indstate.edu>
2018-12-04 03:49:26 -0500

    Connection handlers added for socket

--- server/connections.py
+++ server/connections.py
@@ -1,13 +1,31 @@
 
+import json
+
 users = {}
 
 def add_user(socket):
     global users
+    print("connection made:", socket)
     users[socket] = {}
 
 def remove_user(socket):
     global users
+    print("connection dropped:", socket)
     del users[socket]
 
 def process_message(socket, message):
-    print(message)
+    global users
+    message = json.loads(message)
+    if message["type"] == "connect":
+        users[socket]["username"] = message["username"]
+    elif message["type"] == "getavailable":
+        send_all_available_users(socket)
+
+def send_all_available_users(socket):
+    global users
+    usernames = []
+    print(users)
+    for s, u in users.items():
+        if s != socket and "username" in u and "opponent" not in u:
+            usernames.append(u["username"])
+    socket.write_message(json.dumps(usernames))
